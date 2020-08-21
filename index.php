ea0f5d7cc7767817283747715088b0bd3b32007e
c4v4r0n <ribeirivinicius26@gmail.com>
2020-08-20 21:22:38 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,32 @@
+<form action="index.php" method="POST">
+	<input type="text" name="username" placeholder="username"><br>
+	<input type="text" name="content" placeholder="comment"><br>
+	<input type="hidden" name="newcomment" value="1">
+	<input type="submit"><br>
+</form>
+<?php
+$db = new SQLite3('data.sqlite', SQLITE3_OPEN_CREATE | SQLITE3_OPEN_READWRITE);
+//Create table
+$db->query('CREATE TABLE IF NOT EXISTS "comments" (
+	"id" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+	"username" VARCHAR,
+	"comment" VARCHAR
+)');
+
+if($_POST['newcomment'] == 1)
+{
+	$db->exec('BEGIN');
+	$db->query('INSERT INTO "comments" ("username", "comment")
+		VALUES ("'. $_POST['username'] .'", "'. $_POST['content'] .'")');
+	$db->exec('COMMIT');
+}
+
+$statement = $db->prepare('SELECT username, comment from "comments"');
+$result = $statement->execute();
+echo "<h2>Comments</h2>";
+
+while($row=$result->fetchArray()){
+	echo "<h4>" . $row[0]. "</h4>" ;
+	echo $row[1];
+}
+?>
